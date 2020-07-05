06cf02888267ccf830f8725eb41392c46bdce732
endercz <droidender@gmail.com>
2020-07-05 17:44:10 +0200

    # Add data.tests project and Person and Role data classes.

--- Code/OrganizationStructure.Data/Person.cs
+++ Code/OrganizationStructure.Data/Person.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace OrganizationStructure.Data
+﻿namespace OrganizationStructure.Data
 {
   public class Person
   {
---
+++ Tests/OrganizationStructure.Data.Tests/TestBase.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrganizationStructure.Data.Tests
+{
+  [TestClass]
+  public class TestBase
+  {
+    [TestInitialize]
+    public void TestInitialize()
+    {
+    }
+
+    [TestCleanup]
+    public void TestCleanup()
+    {
+    }
+  }
+}
